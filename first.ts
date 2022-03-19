af4e65fc1bec407937c744880ef032db077bb40f
webfaisalbd <web.faisal.bd@gmail.com>
2022-03-19 15:52:14 +0600

    number,string,boolean

--- first.ts
+++ first.ts
@@ -1,6 +1,22 @@
-console.log("Hello Typescript");
-const a=5;
-const b=a+7;
-console.log("The output is: ",a+b);
-const myName="Faisal"
-console.log(myName);
+// console.log("Hello Typescript");
+// const a=5;
+// const b=a+7;
+// console.log("The output is: ",a+b);
+// const myName="Faisal"
+// console.log(myName);
+
+// Type
+
+// first code 
+let studentName = 'Faisal';
+let studentId=5;
+let goSchool=true;
+console.log('My name is '+studentName+ ' and my id is '+studentId+' and go school? ans:  '+goSchool);
+
+// second code 
+// No problem, because let diye ami variable assign koresi but value set kori nai.
+let officeName;
+officeName='IT firm'
+officeName=50000;
+console.log(officeName);
+
--- js/first.js
+++ js/first.js
@@ -1,7 +1,17 @@
 "use strict";
-console.log("Hello Typescript");
-const a = 5;
-const b = a + 7;
-console.log("The output is: ", a + b);
-const myName = "Faisal";
-console.log(myName);
+// console.log("Hello Typescript");
+// const a=5;
+// const b=a+7;
+// console.log("The output is: ",a+b);
+// const myName="Faisal"
+// console.log(myName);
+let studentName = 'Faisal';
+let studentId = 5;
+let goSchool = true;
+console.log('My name is ' + studentName + ' and my id is ' + studentId + ' and go school? ans:  ' + goSchool);
+// second code 
+// No problem, because let diye ami variable assign koresi but value set kori nai.
+let officeName;
+officeName = 'IT firm';
+officeName = 50000;
+console.log(officeName);
